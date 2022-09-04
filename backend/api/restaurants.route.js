483ef30ba30f866bf31e3d23138272643e1c7cba
shrikarthik007 <holebagilshrikarthik@gmail.com>
2022-09-04 18:48:16 +0530

    commit

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -5,8 +5,8 @@ import ReviewsCtrl from "./reviews.controller.js";
 const router = express.Router();
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
-router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById);
-router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines);
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantsById);
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantsCuisines);
 
 router
   .route("/review")
--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -3,7 +3,7 @@ import ReviewsDAO from "../dao/reviewsDAO.js";
 export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
-      const restaurantId = req.body.text.toString();
+      const restaurantId = req.body.text;
       const review = req.body.text;
       const userInfo = {
         name: req.body.name,
